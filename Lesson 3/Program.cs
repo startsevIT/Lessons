d8e6f36f021ecc3e121fe53483ecf05e985fb478
Kirill_Maksimovich <Kirill_Maksimovich@DESKTOP-UQB6MN8>
2023-09-07 22:04:15 +0300

    Добавлены урок 3 и урок 4

---
+++ Lesson 3/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    //УСЛОВНЫЕ ВЫРАЖЕНИЯ
+    //Операции сравнения
+    //==,!=,<,>,<=,>=
+
+    //Логические операции
+    // |,&,||,&&,!,^
+    //Разница между двойными операторами и одинарными;
+
+    //УСЛОВНЫЕ КОНСТРУКЦИИ
+    //if(Условие){},
+    //if(Условие){}else{},
+    //if(Условие){}esle if(Условие){}else
+
+    //Тернарная операция
+    //условие ? значение_если_правда : значение_если_ложь;
+    private static void Main(string[] args)
+    {
+        bool a = true;
+        bool b = true;
+        string msg = "{0}\t|\t{1}\t|\t{2}\t|\t{3}";
+        Console.Title = "Алгебра логики";
+
+        Console.WriteLine("\nОператор OR '||'\n");
+        Console.WriteLine("FIRST\t|\tOP\t|\tSECOND\t|\tRESULT");
+        Console.WriteLine("---------------------------------------------------------");
+        Console.WriteLine(msg, a = true,"||", b = true, a || b);
+        Console.WriteLine(msg, a = true, "||", b = false, a || b);
+        Console.WriteLine(msg, a = false, "||", b = true, a || b);
+        Console.WriteLine(msg, a = false, "||", b = false, a || b);
+
+        Console.WriteLine("\nОператор AND '&&'\n");
+        Console.WriteLine("FIRST\t|\tOP\t|\tSECOND\t|\tRESULT");
+        Console.WriteLine("---------------------------------------------------------");
+        Console.WriteLine(msg, a = true, "&&", b = true, a && b);
+        Console.WriteLine(msg, a = true, "&&", b = false, a && b);
+        Console.WriteLine(msg, a = false, "&&", b = true, a && b);
+        Console.WriteLine(msg, a = false, "&&", b = false, a && b);
+
+        Console.WriteLine("\nОператор XOR '^'\n");
+        Console.WriteLine("FIRST\t|\tOP\t|\tSECOND\t|\tRESULT");
+        Console.WriteLine("---------------------------------------------------------");
+        Console.WriteLine(msg, a = true, "^", b = true, a ^ b);
+        Console.WriteLine(msg, a = true, "^", b = false, a ^ b);
+        Console.WriteLine(msg, a = false, "^", b = true, a ^ b);
+        Console.WriteLine(msg, a = false, "^", b = false, a ^ b);
+        Console.ReadKey();
+    }
+}
---
+++ Lesson 4/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    //КОНСОЛЬ
+    //Console.BackgroundColor и ConsoleColor
+    //Console.ForegroundColor и ConsoleColor
+    //Console.Beep();
+
+    //ЦИКЛЫ
+    //for, do...while,while,foreach(строки)
+    //continue, break
+    //Вложенные циклы
+
+    static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine("\t\t\tТАБЛИЦА УМНОЖЕНИЯ\n");
+        for (int i = 1; i < 10; i++)
+        {
+            for (int j = 1; j < 10; j++)
+            {
+                if (i == 1 || j == 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.Beep();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.Write(i*j + "\t");
+            }
+            Console.WriteLine("\n");
+        }
+        Console.ReadKey();
+    }
+}
